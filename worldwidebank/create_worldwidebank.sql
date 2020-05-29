9fefb9d3fc7c4887913553fff2f665c83e27e11d
Christopher Perro <cmperro@gmail.com>
2020-05-29 11:05:24 -0400

    Update create_worldwidebank.sql

--- worldwidebank/create_worldwidebank.sql
+++ worldwidebank/create_worldwidebank.sql
@@ -362,7 +362,7 @@ STORED AS ORC
 tblproperties("transactional"="false");
 
 
-INSERT INTO cost_savings.claim_savings SELECT * FROM cost_savings.claim_savings_temp;
+INSERT OVERWRITE TABLE cost_savings.claim_savings SELECT * FROM cost_savings.claim_savings_temp;
 
 DROP TABLE cost_savings.claim_savings_temp;
 
